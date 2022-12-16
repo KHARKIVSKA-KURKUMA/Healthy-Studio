57110f15daf22ab9ef0412991a5dcbab880383cf
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2022-12-16 13:54:23 +0200

    fix

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,9 @@
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 2,
-});
 $('.multiple-items-mob').slick({
   infinite: true,
   slidesToShow: 1,
+});
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
 });
