9f0b590edd1520dabbbbb120f37366cb4ea91cf5
KHARKIVSKA-KURKUMA <99280393+KHARKIVSKA-KURKUMA@users.noreply.github.com>
2023-04-07 21:36:19 +0000

    Deploying to gh-pages from @ KHARKIVSKA-KURKUMA/Healthy-Studio@1dc2978adaa3fec6cebade80fa09014b9956a2ff 🚀

---
+++ index.86c7276c.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({slidesToShow:2,infinite:!0});
+//# sourceMappingURL=index.86c7276c.js.map
---
+++ index.94d1926b.js
@@ -0,0 +1,2 @@
+$(".multiple-items-mob").slick({infinite:!0,slidesToShow:1}),$(".instagram-slider").slick({infinite:!0,slidesToShow:3});
+//# sourceMappingURL=index.94d1926b.js.map
